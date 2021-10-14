dbc397b04c4ef20c6f19f3a0871de9a2dfe87c42
Hank Li <HankLi0130@gmail.com>
2021-10-14 14:15:48 +0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# 玩魔物獵人學設計模式
+
+![](https://game.capcom.com/manual/MHRISE/img/common/logo.png)
+
+## 鐵蟲絲招式 - 策略模式 (Strategy)
+
+![](https://game.capcom.com/manual/MHRISE/locale/page/101_5_1.jpg)
+
--- src/main/kotlin/app/hankdev/designpattern/strategy/StrategyTest.kt
+++ src/main/kotlin/app/hankdev/designpattern/strategy/StrategyTest.kt
@@ -1,7 +1,12 @@
 package app.hankdev.designpattern.strategy
 
 fun main() {
-    val silkbind: Silkbind = GreatSwordSilkbind()
+    // 鐵絲蟲技
+    // val silkbind: Silkbind = GreatSwordSilkbind()    // 大劍鐵絲蟲技
+    // val silkbind: Silkbind = LongSwordSilkbind()     // 太刀鐵絲蟲技
+    val silkbind: Silkbind = BowSilkbind()              // 弓鐵絲蟲技
+
+    // 各個角色裝備鐵絲蟲技
     val role = Role(silkbind)
     role.silkbindAttack()
 }
